b4f850c53c84d0d1264f6702fff0c769071178ca
lamarn <lev.perschin@gmail.com>
2016-06-06 20:34:33 +0200

    Added some MDL and tried to design a little bit :)

--- index.php
+++ index.php
@@ -2,14 +2,30 @@
 <html>
 <head>
     <title>LsW Mitglieder - Übersicht</title>
+    <!-- my own stylesheet -->
     <link rel="stylesheet" href="stylesheet.css">
+    <!-- JQuery -->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <!-- MDL -->
+    <script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.indigo-pink.min.css">
+    <!-- for different devices -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<body>
-<p id="title">Das LsW-Team</p>
-<p id="subtitle">Hier seht ihr unser Linux-statt-Windows-Team</p>
-<div class=page>
-    <?php include 'showMember.php'; ?>
+<body class="mdl-color--grey-100 mdl-color-text--grey-700">
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header mdl-layout__header--seamed mdl-color--primary">
+        <div class="mdl-layout__header-row"><h3>LsW - Team</h3></div>
+        <div class="mdl-layout--large-screen-only mdl-layout__header-row">
+            <h4>Eine kleine Übersicht des gesamten Teams von lsw.io</h4>
+        </div>
+        <br>
+    </header>
+    <main class="mdl-layout__content mdl-base">
+        <?php include 'showMember.php'; ?>
+    </main>
 </div>
 </body>
 </html>
+
--- showMember.php
+++ showMember.php
@@ -1,20 +1,22 @@
 <?php
-$memberList = file_get_contents('member.json');
+$memberList = file_get_contents('members.json');
 $memberList_json = json_decode($memberList);
 
 foreach ($memberList_json->members as $key) {
     echo
-        "<div class='card'>"
-        . $key->name . "<br/>"
-        . $key->age . "<br/>"
-        . $key->project . "<br/>"
-        . $key->hometown . "<br/>"
-        . $key->email . "<br/>"
-        . $key->facebook . "<br/>"
-        . $key->twitter . "<br/>"
-        . $key->google . "<br/>"
-        . $key->profilePic . "<br/>"
-        . $key->backgroundPic . "<br/>"
-        . "</div>";
+        "<section class='section--center mdl-shadow--2dp my-section'>"
+        . "<ul class='mdl-list'>"
+        . "<li>" . "<b>" . $key->name . "</b>" . "</li>"
+        . "<li>" . "<span class='left'>" . "Alter: " . "</span>" . "<span class='right'>" . $key->age . "</span>" . "</li>"
+        . "<li>" . "Abteilung: " . $key->project . "</li>"
+        . "<li>" . $key->hometown . "</li>"
+        . "<li>" . $key->email . "</li>"
+        . "<li>" . $key->facebook . "</li>"
+        . "<li>" . $key->twitter . "</li>"
+        . "<li>" . $key->google . "</li>"
+        . "<li>" . $key->profilePic . "</li>"
+        . "<li>" . $key->backgroundPic . "</li>"
+        . "</ul>"
+        . "</section>";
 }
 ?>
